4797de75144f7de56b06fe912a140de2dbae0982
bspatafora <benjamin.spatafora@gmail.com>
2014-09-10 15:11:02 -0500

    Add handling of invalid command-line arguments to CommandLine’s parseArguments method

--- src/com/bspatafora/java_server/CommandLine.java
+++ src/com/bspatafora/java_server/CommandLine.java
@@ -6,12 +6,20 @@ import java.util.Map;
 public class CommandLine {
     public static Map parseArguments(String[] args) {
         Map<String, Object> argMap = new HashMap<>();
-        if (args[0].equals("-p")) {
-            argMap.put("port", Integer.parseInt(args[1]));
-            argMap.put("directory", args[3]);
-        } else {
-            argMap.put("port", Integer.parseInt(args[3]));
-            argMap.put("directory", args[1]);
+        try {
+            if (args[0].equals("-p")) {
+                argMap.put("port", Integer.parseInt(args[1]));
+                argMap.put("directory", args[3]);
+            } else if (args[0].equals("-d")) {
+                argMap.put("port", Integer.parseInt(args[3]));
+                argMap.put("directory", args[1]);
+            } else {
+                argMap.put("port", 5000);
+                argMap.put("directory", "/default/directory/");
+            }
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            argMap.put("port", 5000);
+            argMap.put("directory", "/default/directory/");
         }
         return argMap;
     }
--- src/com/bspatafora/java_server/CommandLineTest.java
+++ src/com/bspatafora/java_server/CommandLineTest.java
@@ -10,21 +10,51 @@ public class CommandLineTest {
 
     @Test
     public void testParseArgumentsPortFirst() throws Exception {
-        String[] args = { "-p", "5000", "-d", "/some_directory/" };
+        String[] args = { "-p", "9000", "-d", "/some_directory/" };
         Map argMap = CommandLine.parseArguments(args);
         Integer port = (Integer) argMap.get("port");
         String directory = (String) argMap.get("directory");
-        assertTrue("Port is correctly parsed when first", port == 5000);
+        assertTrue("Port is correctly parsed when first", port == 9000);
         assertTrue("Directory is correctly parsed when second", directory.equals("/some_directory/"));
     }
 
     @Test
     public void testParseArgumentsDirectoryFirst() throws Exception {
-        String[] args = { "-d", "/some_directory/", "-p", "5000" };
+        String[] args = { "-d", "/some_directory/", "-p", "9000" };
         Map argMap = CommandLine.parseArguments(args);
         Integer port = (Integer) argMap.get("port");
         String directory = (String) argMap.get("directory");
-        assertTrue("Port is correctly parsed when second", port == 5000);
+        assertTrue("Port is correctly parsed when second", port == 9000);
         assertTrue("Directory is correctly parsed when first", directory.equals("/some_directory/"));
+    }
+
+    @Test
+    public void testParseArgumentsMalformed() throws Exception {
+        String[] args = { "-r", "incorrect", "-g", "usage" };
+        Map argMap = CommandLine.parseArguments(args);
+        Integer port = (Integer) argMap.get("port");
+        String directory = (String) argMap.get("directory");
+        assertTrue("Port defaults to 5000 with malformed args", port == 5000);
+        assertTrue("Directory defaults to '/default/directory/' with malformed args", directory.equals("/default/directory/"));
+    }
+
+    @Test
+    public void testParseArgumentsTooFew() throws Exception {
+        String[] args = { "-d", "/some_directory/" };
+        Map argMap = CommandLine.parseArguments(args);
+        Integer port = (Integer) argMap.get("port");
+        String directory = (String) argMap.get("directory");
+        assertTrue("Port defaults to 5000 with too few args", port == 5000);
+        assertTrue("Directory defaults to '/default/directory/' with too few args", directory.equals("/default/directory/"));
+    }
+
+    @Test
+    public void testParseArgumentsPortInvalid() throws Exception {
+        String[] args = { "-p", "not_a_number", "-d", "/some_directory/" };
+        Map argMap = CommandLine.parseArguments(args);
+        Integer port = (Integer) argMap.get("port");
+        String directory = (String) argMap.get("directory");
+        assertTrue("Port defaults to 5000 with invalid port", port == 5000);
+        assertTrue("Directory defaults to '/default/directory/' with invalid port", directory.equals("/default/directory/"));
     }
 }
